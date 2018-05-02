a86344f140a74d25947d6aa411ce06f20627d142
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-02 17:52:00 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word =~ /^[aeiouAEIOU]/
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun*\S*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text =~ /\A[A-Z]+[^.!?]*[.!?]$/
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone =~ /^\D?(\d\D?){10}/
+    true
+  else
+    false
+  end
 end
